a21d45039084eef068e132ff29db10977387f4db
Eric Elsken <eric.elsken@gmail.com>
2022-04-15 14:50:17 -0700

    Delete bash directory

--- bash/functions.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mkcd () {
-    mkdir -p "$1" && cd "$1"
-}
--- bash/history.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export HISTSIZE=1000
-export HISTFILESIZE=5000
-export HISTCONTROL=ignorespace:ignoredups
-export HISTIGNORE="ls:ls -la:l:la:cd:cd -:exit:clear:q:git:sudo su -"
